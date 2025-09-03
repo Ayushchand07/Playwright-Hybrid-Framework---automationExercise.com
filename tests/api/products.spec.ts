662003a4bb9637785598fb4fc5dae2b591dae318
Ayush Chand <aayushchand900@gmail.com>
2025-09-03 23:26:35 +0530

    apitesting

--- tests/api/products.spec.ts
+++ tests/api/products.spec.ts
@@ -11,8 +11,6 @@ import postAllProductsBody from '../../testData/apiTestData/postAllProducts.json
 import getAllBrandsBody from '../../testData/apiTestData/getAllBrands.json'
 import getAllBrandsSchema from '../../testData/schemas/getAllBrands.schema.json'
 import putAllBrandsBody from '../../testData/apiTestData/putAllBrands.json'
-import loginWithoutEmailBody from '../../testData/apiTestData/loginWithoutEmail.json'
-import deleteLoginBody from '../../testData/apiTestData/deleteLogin.json'
 
 dotenv.config();
 
@@ -69,28 +67,6 @@ test('API 4: PUT To All Brands List', async({request})=>{
 //     await
 // })
 
-test('API 7: POST To Verify Login with valid details', async({request})=>{
-    const response = await client.putAllBrands()
-    await statusCodeValidator.validateStatusCode(response,200)
-    await responseCodeValidator.validateResponseCode(response,405)
-    await responseBodyValidator.validateResponseBody(response,putAllBrandsBody)
-})
-
-
-test('API 8: POST To Verify Login without email parameter', async({request})=>{
-    const response = await client.putAllBrands()
-    await statusCodeValidator.validateStatusCode(response,200)
-    await responseCodeValidator.validateResponseCode(response,400)
-    await responseBodyValidator.validateResponseBody(response,loginWithoutEmailBody)
-})
-
-
-test('API 9: DELETE To Verify Login', async({request})=>{
-    const response = await client.putAllBrands()
-    await statusCodeValidator.validateStatusCode(response,200)
-    await responseCodeValidator.validateResponseCode(response,405)
-    await responseBodyValidator.validateResponseBody(response,deleteLoginBody)
-})
 
 
 
